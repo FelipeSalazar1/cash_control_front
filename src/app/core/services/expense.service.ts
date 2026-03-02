ea9822c043cb2deb756f89ce0e94ca9b72df4d23
Felipe <felipe.salazar2004@outlook.com>
2026-03-02 11:55:16 -0300

    Fix: api_key

--- src/app/core/services/expense.service.ts
+++ src/app/core/services/expense.service.ts
@@ -3,8 +3,9 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 import { Category, Expense } from '../models/expense.model';
+import { environment } from '../../../environments/environment';
 
-const API_BASE_URL = 'http://localhost:8080/api/v1';
+const API_BASE_URL = environment.API_BASE_URL;
 
 @Injectable({
   providedIn: 'root'
